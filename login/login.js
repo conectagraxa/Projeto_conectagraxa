b5f343a6c5eb41099c7d0ddcd2c556c14c94afb8
Leandro <leandromarquesdeusefiel@gmail.com>
2023-10-04 01:14:19 -0300

    first commit

---
+++ login/login.js
@@ -0,0 +1,13 @@
+document.getElementById('olho').addEventListener('mousedown', function () {
+  document.getElementById('pass').type = 'text';
+});
+
+document.getElementById('olho').addEventListener('mouseup', function () {
+  document.getElementById('pass').type = 'password';
+});
+
+document.getElementById('olho').addEventListener('mousemove', function () {
+  document.getElementById('pass').type = 'password';
+});
+
+
