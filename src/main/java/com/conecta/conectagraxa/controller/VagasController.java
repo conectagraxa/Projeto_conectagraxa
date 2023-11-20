5207017e5a8c069c4d1a5f576a9fe7b34fbf9057
Suelen Feijo <conectagraxa@gmail.com>
2023-11-20 04:09:27 -0300

    adicionado endpoint editar vaga

--- src/main/java/com/conecta/conectagraxa/controller/VagasController.java
+++ src/main/java/com/conecta/conectagraxa/controller/VagasController.java
@@ -6,12 +6,15 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.conecta.conectagraxa.model.Postagens;
 import com.conecta.conectagraxa.model.Vagas;
 import com.conecta.conectagraxa.model.dto.PostagensDTO;
 import com.conecta.conectagraxa.model.dto.VagasDTO;
@@ -27,12 +30,17 @@ public class VagasController {
 	// CRIAR VAGA
 	@PostMapping(value = "/create")
 	public ResponseEntity<VagasDTO> create(@Valid @RequestBody VagasDTO objDTO) throws Exception {
-		Vagas newObj = service.createVaga(objDTO, objDTO.getEmpresaId());
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
-		return ResponseEntity.created(uri).build();
+	Vagas newObj = service.createVaga(objDTO, objDTO.getEmpresaId());
+	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+	return ResponseEntity.created(uri).build();
 	}
 
 	// EDITAR VAGA
+	@PutMapping("/update-postagem/{id}")
+	public ResponseEntity<VagasDTO> update(@PathVariable Integer id, @RequestBody @Valid VagasDTO objDTO)throws Exception {
+	Vagas obj = service.editarVaga( objDTO,id);
+	return ResponseEntity.ok().body(new VagasDTO(obj));
+	}
 
 	// FILTRAR VAGA CATEGORIA
 
@@ -46,7 +54,7 @@ public class VagasController {
 
 	// PROFISSIONAL VISUALIZAR VAGAS CANDIDATADAS
 
-	// EMPRESA VISUALIZAR FEED PROFISSIONAL CANDIDATADO NA VAGA 
+	// EMPRESA VISUALIZAR FEED PROFISSIONAL CANDIDATADO NA VAGA
 
 	// PROFISSIONAL VISUALIZAR FEED EMPRESA CANDIDATADA
 
