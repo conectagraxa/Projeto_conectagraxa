106cce83b4b982fae6f6ace2556270ecb8cf26f8
Suelen Feijo <conectagraxa@gmail.com>
2023-11-21 05:50:17 -0300

    services vagas concluídos.

--- src/main/java/com/conecta/conectagraxa/service/VagasService.java
+++ src/main/java/com/conecta/conectagraxa/service/VagasService.java
@@ -1,7 +1,5 @@
 package com.conecta.conectagraxa.service;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -9,7 +7,6 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import com.conecta.conectagraxa.model.Categoria;
@@ -20,173 +17,242 @@ import com.conecta.conectagraxa.model.dto.VagasDTO;
 import com.conecta.conectagraxa.repositories.CategoriaRepository;
 import com.conecta.conectagraxa.repositories.VagasRepository;
 
+/*
+ * @version 1.0
+ * @Param serviço do controlador excluir, editar, pesquisar, filtrar, criar vagas 
+ */
 @Service
 public class VagasService {
 
-	
+	/*
+	 * @Param Repositório Vagas
+	 */
 	@Autowired
 	VagasRepository repository;
-	
+
+	/*
+	 * @Param Repositório Categoria
+	 */
 	@Autowired
 	CategoriaRepository cRepository;
-	
+
+	/*
+	 * @Param Service Empresa
+	 */
 	@Autowired
 	EmpresaService empService;
-	
+
+	/*
+	 * @Param Service Categoria
+	 */
 	@Autowired
 	CategoriaService cService;
-	
-	
-	
+	/*
+	 * @Param Service Feed
+	 */
 	@Autowired
 	Feed_EmpresaService feedService;
-	
-	//CRIAR VAGA
-	public Vagas createVaga(VagasDTO obj, Integer id) throws Exception {
-		//procurando se o id da empresa existe	
+
+	/*
+	 * @param CRIAR VAGA
+	 * parâmetro : VagasDTO, Integer
+	 */
+	public Vagas createVaga(VagasDTO vaga, Integer id) throws Exception {
+		// procurando se o id da empresa e feed se existe
 		Feed_Empresa feedEmpresa = feedService.findById(id);
-		Empresa empresa = empService.findById(id);	
+		Empresa empresa = empService.findById(id);
 		
-		Categoria categoria = cService.CategoriaById(obj.getCategoriaId());
-		categoria.setId(obj.getCategoriaId());
-		obj.setCategoriaId(obj.getCategoriaId());
-		Vagas vagas = new Vagas(obj);
-		
-		//setando os dados passados no vagasDTO (Oque vai vai no corpo da requisição)
-		vagas.setTitulo(obj.getTitulo());
-		vagas.setFotoPath(obj.getFotoPath());
-		vagas.setDescricao(obj.getDescricao());
-		vagas.setCidade(obj.getCidade());
-		vagas.setEstado(obj.getEstado());
+		//verificando se categoria existe
+		Categoria categoria = cService.CategoriaById(vaga.getCategoriaId());
+		categoria.setId(vaga.getCategoriaId());
+		vaga.setCategoriaId(vaga.getCategoriaId());
+		Vagas vagas = new Vagas(vaga);
+
+		// setando os dados passados no vagasDTO (Oque vai vai no corpo da requisição)
+		vagas.setTitulo(vaga.getTitulo());
+		vagas.setFotoPath(vaga.getFotoPath());
+		vagas.setDescricao(vaga.getDescricao());
+		vagas.setCidade(vaga.getCidade());
+		vagas.setEstado(vaga.getEstado());
 		vagas.setDataPostagem(vagas.getDataPostagem());
-		//seta feed e empresa
+		// seta feed e empresa
 		vagas.setFeedEmpresaId(feedEmpresa);
 		vagas.setEmpresaId(empresa);
 		vagas.setCategoriaId(categoria);
-		//adiciona a vaga setada no feed da empresa
+		// adiciona a vaga setada no feed da empresa
 		categoria.getVagas().add(vagas);
 		feedEmpresa.getVagas().add(vagas);
-		//salva no banco
-		Vagas newObj = repository.save(vagas);
+		// salva no banco
+		Vagas novaVaga = repository.save(vagas);
 
-		//retorna um model vagas com valor atualizado
-		return newObj;
-		
+		// retorna um model vagas com valor atualizado
+		return novaVaga;
+
 	}
 
-
-	// EDITAR VAGA
-	public Vagas editarVaga(VagasDTO obj, Integer id) throws Exception {
-		//procurando se o id da vaga e empresa existe	
+	/*
+	 * @Param esse método EDITA UMA VAGA no corpo da requisição (vagaDTO)
+	 *  Integer é usado para associar ao feed e verificação se existe o feed e empresa. 
+	 */
+	public Vagas editarVaga(VagasDTO vagaDTO, Integer id) throws Exception {
+		/*
+		 * procurando se o id da vaga e empresa existe	
+		 */
 		Feed_Empresa feedEmpresa = feedService.findById(id);
 		Empresa empresa = empService.findById(id);
-		Categoria categoria = cService.CategoriaById(obj.getCategoriaId());
-		Vagas newObj = new Vagas();
+		Categoria categoria = cService.CategoriaById(vagaDTO.getCategoriaId());
+		Vagas novaVaga = new Vagas();
 		Optional <Vagas> vagas = repository.findById(id);
 		if(vagas.isPresent()) {
-		newObj.setId(vagas.get().getId());
+		novaVaga.setId(vagas.get().getId());
 
-		//leitura informações já existentes
-		//recuperando e verificando se os dados encontrados no
-		//banco estão vazios, se não estiver, pega a informação e mantém no newObj
+		/* 
+		 *	recuperando e verificando se os dados encontrados no
+		 *	banco estão vazios, se não estiver, pega a informação e mantém no newObj
+		 *	
+		 *  leitura informações já existentes
+		 */
 		if (vagas.get().getCategoriaId() != null) {
-			newObj.setCategoriaId(vagas.get().getCategoriaId());
+			novaVaga.setCategoriaId(vagas.get().getCategoriaId());
 		}
 		if (vagas.get().getTitulo() != null) {
-			newObj.setTitulo(vagas.get().getTitulo());
+			novaVaga.setTitulo(vagas.get().getTitulo());
 		}
 		if (vagas.get().getFotoPath() != null) {
-			newObj.setFotoPath(vagas.get().getFotoPath());
+			novaVaga.setFotoPath(vagas.get().getFotoPath());
 		}
 		if (vagas.get().getDescricao() != null) {
-			newObj.setDescricao(vagas.get().getDescricao());
+			novaVaga.setDescricao(vagas.get().getDescricao());
 		}
 		if (vagas.get().getCidade() != null) {
-			newObj.setCidade(vagas.get().getCidade());
+			novaVaga.setCidade(vagas.get().getCidade());
 		}
 		if (vagas.get().getEstado() != null) {
-			newObj.setEstado(vagas.get().getEstado());
+			novaVaga.setEstado(vagas.get().getEstado());
 		
 		}if(vagas.get().getCategoriasUp() !=null) {
-			newObj.setCategoriasUp(vagas.get().getCategoriasUp());
+			novaVaga.setCategoriasUp(vagas.get().getCategoriasUp());
 		}
-		newObj.setDataPostagem(vagas.get().getDataPostagem());
-		newObj.setFeedEmpresaId(vagas.get().getFeedEmpresaId());
-		newObj.setEmpresaId(vagas.get().getEmpresaId());
-		newObj.setCandidaturas(vagas.get().getCandidaturas());
+		novaVaga.setDataPostagem(vagas.get().getDataPostagem());
+		novaVaga.setFeedEmpresaId(vagas.get().getFeedEmpresaId());
+		novaVaga.setEmpresaId(vagas.get().getEmpresaId());
+		novaVaga.setCandidaturas(vagas.get().getCandidaturas());
 		
 		//input novas informações
 		//recuperando e verificando se os campos do update na requisição
 		//estão vazios, se não estiver, pega a informação e seta no newObj
 		
-		if (obj.getCategoriaId() != null) {
-			newObj.setCategoriaId(categoria);
+		if (vagaDTO.getCategoriaId() != null) {
+			novaVaga.setCategoriaId(categoria);
 		}
-		if (obj.getTitulo() != null) {
-			newObj.setTitulo(obj.getTitulo());
+		if (vagaDTO.getTitulo() != null) {
+			novaVaga.setTitulo(vagaDTO.getTitulo());
 		}
-		if (obj.getFotoPath() != null) {
-			newObj.setFotoPath(obj.getFotoPath());
+		if (vagaDTO.getFotoPath() != null) {
+			novaVaga.setFotoPath(vagaDTO.getFotoPath());
 		}
-		if (obj.getDescricao() != null) {
-			newObj.setDescricao(obj.getDescricao());
+		if (vagaDTO.getDescricao() != null) {
+			novaVaga.setDescricao(vagaDTO.getDescricao());
 		}
-		if (obj.getCidade() != null) {
-			newObj.setCidade(obj.getCidade());
+		if (vagaDTO.getCidade() != null) {
+			novaVaga.setCidade(vagaDTO.getCidade());
 		}
-		if (obj.getEstado() != null) {
-			newObj.setEstado(obj.getEstado());
-		}if(obj.getCategoriaId() !=null) {
+		if (vagaDTO.getEstado() != null) {
+			novaVaga.setEstado(vagaDTO.getEstado());
+		}if(vagaDTO.getCategoriaId() !=null) {
 			
-			newObj.setCategoriasUp(obj.getCategoriaId());
+			novaVaga.setCategoriasUp(vagaDTO.getCategoriaId());
 		}
 		
 		
 		//salva update no banco
-		newObj = repository.save(newObj);
+		novaVaga = repository.save(novaVaga);
 		//retorna um model vagas com valor atualizado
 		}
-		return newObj;
+		return 	novaVaga;
 		
 	}
-	//FILTRAR VAGA CATEGORIA 
+
 //	@Query("SELECT categoria FROM vagas WHERE categoria LIKE '%1%'")
-	//@Query("select vagas.titulo from vagas where vagas.categoriaId = :categoriaId")
+//@Query("select vagas.titulo from vagas where vagas.categoriaId = :categoriaId")
+	/*
+	 * FILTRAR VAGA CATEGORIA
+	 * 
+	 * @Param filtro por categoria mocada no banco de dados, categorias com id de 1
+	 * ao 5. 1 - todos | 2 - Áudio e Som | 3 - Iluminação | 4 - Palco | 5 -
+	 * Equipamentos
+	 * 
+	 * @Param Parâmetro = Integer , usado para verificar se a categoria existe por
+	 * id
+	 * 
+	 * @return isPresent: filtragem por id da categoria, listando todas as vagas de
+	 * acordo.
+	 * 
+	 * @return não isPresent: lista vazia.
+	 */
 	@Transactional
 	public List<Vagas> findCategoria(Integer categoriaId) throws Exception {
-		Optional <Categoria> v1 = cRepository.findById(categoriaId);
-	   // List<Vagas> obj = repository.findAll();
-	    
-	    
-	    if (v1.isPresent()) {
-	    	return	    //	repository.findByCategoriaIdContaining(v1.get());
-	    	repository.findByCategoriasUp(categoriaId);
+		Optional<Categoria> vagas = cRepository.findById(categoriaId);
+		// List<Vagas> obj = repository.findAll();
 
-	    }
-	    return new ArrayList<>();
+		if (vagas.isPresent()) {
+			return // repository.findByCategoriaIdContaining(v1.get());
+			repository.findByCategoriasUp(categoriaId);
+
+		}
+		return new ArrayList<>();
 	}
 
-	
-	
-	// LISTAR VAGAS POR NOME
+	/*
+	 * @Param LISTA VAGAA POR TITULO
+	 * 
+	 * @Param parâmetro: String
+	 * 
+	 * @return lista de vagas com titulos buscados
+	 */
 	@Transactional
-	public Vagas findByTitulo(String titulo) throws Exception {
-	Optional<Vagas> obj = repository.findByTituloIgnoreCaseContaining(titulo); 
-	return obj.orElseThrow(() -> new Exception("objeto nome encontrado Nome: " + titulo ));
+	public List<Vagas> findByTitulo(String titulo) throws Exception {
+		List<Vagas> vagas = repository.findByTitulo(titulo);
+		return vagas;
+		
 	}
 
-	// DELETAR VAGA
+	/*
+	 * DELETAR VAGA
+	 * 
+	 * @Param para deletar a vaga, nesse método é necessário desassociar as relações
+	 * da classe filha com a classe mãe, esse método deleta uma vaga.
+	 */
 	@Transactional
 	public void delete(Integer id) throws Exception {
-		Optional<Vagas> obj = repository.findById(id);
+		Optional<Vagas> vagas = repository.findById(id);
 
-		if (obj.get().getId().equals(id)) {
+		if (vagas.isPresent()) {
+
+			/*
+			 * removemento relações setando nulos os campos associados a Vagas
+			 */
+			vagas.get().deleteCategoria();
+			vagas.get().deleteCandidaturas();
+			vagas.get().deleteEmpresa();
+			vagas.get().deleteFeedEmpresa();
+
+			/*
+			 * removendo a vaga da lista nas classes pai
+			 */
+			vagas.get().getFeedEmpresaId().deleteVagas();
+			vagas.get().getCategoriaId().deleteVagas();
+			/*
+			 * deletando
+			 */
 			repository.deleteById(id);
 		} else {
+			/*
+			 * execeção caso não encontrada
+			 */
 			throw new Exception("Vaga não encontrada");
 		}
 
 	}
-
 
 }
