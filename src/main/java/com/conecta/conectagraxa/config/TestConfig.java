f7641670990bb81548baa5de432cc7c9d185d681
Suelen Feijo <conectagraxa@gmail.com>
2023-12-05 10:53:41 -0300

    trocando para o banco h2 , salvando em arquivo.

--- src/main/java/com/conecta/conectagraxa/config/TestConfig.java
+++ src/main/java/com/conecta/conectagraxa/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.conecta.conectagraxa.config;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -16,14 +17,22 @@ public class TestConfig {
 	@Autowired
 	DBService service;
 
-	
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+
 	
 	@Bean
-	public void instaciaDB() throws Exception {
-	this.service.instanciaDB();
+	public boolean instaciaDB() throws Exception {
+		if (value.equals("create")/* || value.equals("update")*/) {
+			this.service.instanciaDB();
 
+			}
+			return false;
+	
+	}
 		
 	}		
-	}
+	
 
 
