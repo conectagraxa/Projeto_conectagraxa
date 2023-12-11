f45e8bbd21390602f2875ea991ce68c8744be520
Lívia V. Gomes <114452531+ViviSGG@users.noreply.github.com>
2023-12-11 02:58:32 -0300

    add perfil

--- new-conecta-graxa/src/App.js
+++ new-conecta-graxa/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Cadastro from "./pages/Cadastro";
 import PrincipalPF from "./pages/PrincipalPF";
 import PrincipalPJ from "./pages/PrincipalPJ";
 import CadastroPF from './components/Cadastro_pessoa_fisica/CadastroPF.js'
@@ -9,17 +8,13 @@ import LoginForm from './pages/Login/index.js'
 
 function App() {
   return (
-    // <PrincipalPJ/>
-    // <PrincipalPF/>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/principalPF/*" element={<PrincipalPF />} />
         <Route path="/principalPJ/*" element={<PrincipalPJ />} />
         <Route path='/CadastroPF' element={<CadastroPF />} />
         <Route path='/LoginForm' element={<LoginForm />} />
-
       </Routes>
     </BrowserRouter>
   );
